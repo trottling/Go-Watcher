45b334157e89ffae2491fd6595520490165bd588
trottling <truewasted@yandex.ru>
2024-08-16 19:45:32 +0300

    Done writing loggers

--- src/Config-Reader/read_config.go
+++ src/Config-Reader/read_config.go
@@ -2,9 +2,7 @@ package Config_Reader
 
 import (
 	"Go-Watcher/src"
-	"bufio"
-	"fmt"
-	"log"
+	"encoding/json"
 	"os"
 )
 
@@ -17,24 +15,34 @@ func ValidateConfig() {
 	// Check for config file exist
 	_, err := os.Stat(src.ConfigPath)
 	if os.IsNotExist(err) {
-		src.Log.Fatal("Config file doesn't exist")
+		src.Log.Panic("Config file doesn't exist: " + src.ConfigPath)
 	}
-	file, err := os.Open("file.txt")
+}
+
+func ReadConfig() {
+	src.Log.Info("Config path: " + src.ConfigPath)
+	file, err := os.Open(src.ConfigPath)
 	if err != nil {
-		log.Fatal(err)
+		src.Log.Fatal(err)
 	}
 	defer func() {
 		if err = file.Close(); err != nil {
 			src.Log.Fatal(err)
 		}
 	}()
-	scanner := bufio.NewScanner(file)
 
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+	configFile, err := os.Open(src.ConfigPath)
+	if err != nil {
+		src.Log.Panic("Cannot read config: " + err.Error())
 	}
-}
 
-func ReadConfig() {
+	// Unmarshal the JSON data
+	jsonParser := json.NewDecoder(configFile)
+	err = jsonParser.Decode(&src.Config)
+	if err != nil {
+		src.Log.Panic("Cannot read config: " + err.Error())
+	}
 
+	// Print the config for debugging
+	src.Log.Info("Config:", src.Config)
 }

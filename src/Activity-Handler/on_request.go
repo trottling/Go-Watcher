bdf8bf2ae86cdb6d88ffc91921925cd14abe7afc
trottling <truewasted@yandex.ru>
2024-08-17 20:39:40 +0300

    Done writing proxy server logic

--- src/Activity-Handler/on_request.go
+++ src/Activity-Handler/on_request.go
@@ -1,1 +1,10 @@
 package Activity_Handler
+
+import (
+	"net/http"
+)
+
+func OnRequest(data interface{}) (*http.Request, *http.Response) {
+	// dd
+	return nil, nil
+}
--- src/Activity-Handler/on_response.go
+++ src/Activity-Handler/on_response.go
@@ -1,1 +1,8 @@
 package Activity_Handler
+
+import "net/http"
+
+func OnResponse(data interface{}) (*http.Request, *http.Response) {
+	// dd
+	return nil, nil
+}
